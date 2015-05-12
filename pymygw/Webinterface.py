aaade6ee4bcf85c08f2055163ba29e8c3d1d5913
Hawkes <github@the-hawkes.de>
2015-05-12 18:14:40 +0200

    added a small info webinterface

--- pymygw/Webinterface.py
+++ pymygw/Webinterface.py
@@ -1,13 +1,21 @@
 from flask import Flask, render_template
+from datetime import datetime
 
 import config
 import Database
 
 app = Flask(__name__, template_folder='web/templates', static_folder='web/static')
 
+
+@app.template_filter('timestamp2date')
+def timestamp2date(timestamp):
+    return datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S')
+
+
 @app.route("/")
 def hello():
     db = Database.Database()
     return render_template('index.html',
-                           sensors=db.get())
+                           sensors=db.get(),
+                           config=config)
 
